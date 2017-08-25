ef28ff70207d9435bd03cb3858238c0f9eeb15c7
Guest User <Guest@Epicodus-10D.local>
2017-08-25 10:36:39 -0700

    strt initial logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,35 @@
 // back-end logic
+function Pizza(movie, time, age, quantity) {
+  this.piStyle = style;
+  this.piSize = size;
+  this.piToppings = toppings;
+  this.piQuantity = quantity;
+}
 
+Pizza.prototype.totalPrice = function() {
+  return '$' + ((this.piStyle + this.piSize) - this.piToppings) * this.piQuantity + '.00';
+}
 
 // front-end logic
 $(document).ready(function() {
-    event.preventDefault();
 
+  $("form").submit(function(event) {
+    event.preventDefault();
+    $("ul#receipt").empty(newTicket);
+
+    var inputtedStyle =
+    var inputtedSize =
+    var inputtedToppings =
+    var inputtedQuantity =
+
+    var cartStyle =
+    var cartSize =
+    var cartoppings =
+    var cartQuantity =
+
+    var newOrder = new Order(inputtedStyle, inputtedSize, inputtedToppings, inputtedQuantity);
+
+    $('ul#receipt').append( + newOrder.totalPrice());
   });
+
 });
